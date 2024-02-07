b85dde9be4d3d326307fc6a5a982aac6de8752f0
Ethan Hunt <ethanweihunt@gmail.com>
2024-02-07 09:47:32 -0800

    fix errors highlighted by flake8

--- src/counter.py
+++ src/counter.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 COUNTERS = {}
 
+
 # We will use the app decorator and create a route called slash counters.
 # specify the variable in route <name>
 # let Flask know that the only methods that is allowed to called
@@ -15,9 +16,10 @@ def create_counter(name):
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
     if name in COUNTERS:
-        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
 
 @app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name):
@@ -25,9 +27,10 @@ def update_counter(name):
     app.logger.info(f"Request to update counter: {name}")
     global COUNTERS
     if name not in COUNTERS:
-        return {"Message":f"Counter {name} doesn't exist"}, status.HTTP_404_NOT_FOUND
+        return {"Message": f"Counter {name} doesn't exist"}, status.HTTP_404_NOT_FOUND
     COUNTERS[name] += 1
     return {name: COUNTERS[name]}, status.HTTP_200_OK
+
 
 @app.route('/counters/<name>', methods=['GET'])
 def get_counter(name):
@@ -35,5 +38,5 @@ def get_counter(name):
     app.logger.info(f"Request to get counter: {name}")
     global COUNTERS
     if name not in COUNTERS:
-        return {"Message":f"Counter {name} doesn't exist"}, status.HTTP_404_NOT_FOUND
+        return {"Message": f"Counter {name} doesn't exist"}, status.HTTP_404_NOT_FOUND
     return {name: COUNTERS[name]}, status.HTTP_200_OK
